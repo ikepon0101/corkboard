82fa089641533ecdf76f3cb06b0ce27c36a3649c
ikepon0101 <ikepon0101@gmail.com>
2015-05-18 02:29:34 +0900

    test

--- insertAction.php
+++ insertAction.php
@@ -1,8 +1,9 @@
 <?php
 try {
+	$requestText = mb_strcut($_REQUEST['text'], 0, 200);
 	$pdo = new PDO("mysql:host=127.0.0.1;dbname=corkboard;charset=utf8", "root", "ikepon0101");
 	$stmt = $pdo->prepare("INSERT INTO `comments`(`time`, `text`, `country`) VALUES (now(), :TEXT, :COUNTRY)");
-	$stmt->bindValue(':TEXT', $_REQUEST['text'], PDO::PARAM_STR);
+	$stmt->bindValue(':TEXT', $requestText, PDO::PARAM_STR);
 	$stmt->bindValue(':COUNTRY', $_REQUEST['country'], PDO::PARAM_STR);
 	$result = $stmt->execute();
 } catch (Exception $e){
